19e6fa670748fb5ae34117684661ee117ab3b410
Alim <asanov.alim.i215@gmail.com>
2020-01-27 01:04:16 +0300

    ViewModel классы

--- app/src/main/java/com/alimasanov/unsplash/viewModel/FavoriteFragmentViewModel.kt
+++ app/src/main/java/com/alimasanov/unsplash/viewModel/FavoriteFragmentViewModel.kt
@@ -1,8 +1,19 @@
 package com.alimasanov.unsplash.viewModel
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.alimasanov.unsplash.App
+import com.alimasanov.unsplash.model.roomObj.PhotoRoom
 
 class FavoriteFragmentViewModel: ViewModel() {
 
+    val listPhoto = MutableLiveData<List<PhotoRoom>>().apply {
+        App.db.photoDao().getListPhotos()
+            .observeOn(AndroidSchedulers.mainThread)
+            .subscribe { t -> value = t }
+    }
 
+    private fun dbQuery(){
+
+    }
 }
--- app/src/main/java/com/alimasanov/unsplash/viewModel/PhotoFragmentViewModel.kt
+++ app/src/main/java/com/alimasanov/unsplash/viewModel/PhotoFragmentViewModel.kt
@@ -1,12 +1,8 @@
 package com.alimasanov.unsplash.viewModel
 
-import android.content.Context
-import android.database.sqlite.SQLiteDatabase
-import android.util.Log
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.alimasanov.unsplash.model.Photo
+import com.alimasanov.unsplash.model.pojo.Photo
 import com.alimasanov.unsplash.server.NetworkEndpoints
 import com.alimasanov.unsplash.server.UnsplashClient
 import retrofit2.Call
@@ -18,22 +14,20 @@ class PhotoFragmentViewModel: ViewModel() {
 
     private val networkEndpoints: NetworkEndpoints = UnsplashClient().getUnsplashClient().create()
     private val photos = networkEndpoints.getRandomPhotos(10)
-    private val _listPhoto = MutableLiveData<List<Photo>>()
-    val listPhoto: LiveData<List<Photo>> = _listPhoto
-    private var photosData: List<Photo>? = ArrayList()
 
-    fun initListPhoto(): MutableLiveData<List<Photo>>? {
+    val listPhoto = initListPhoto()
 
-            photos.enqueue(object : Callback<List<Photo>> {
-                override fun onFailure(call: Call<List<Photo>>, t: Throwable) {
-                    Log.d("callback error", t.localizedMessage)
-                }
+    private fun initListPhoto(): MutableLiveData<List<Photo>>? {
+        val listPhotoMut = MutableLiveData<List<Photo>>()
+        photos.enqueue(object : Callback<List<Photo>> {
+            override fun onFailure(call: Call<List<Photo>>, t: Throwable) {
+            }
 
-                override fun onResponse(call: Call<List<Photo>>, response: Response<List<Photo>>) {
-                    _listPhoto.value = response.body()
-                }
-            })
+            override fun onResponse(call: Call<List<Photo>>, response: Response<List<Photo>>) {
+                listPhotoMut.value = response.body()
+            }
+        })
 
-        return _listPhoto
+        return listPhotoMut
     }
 }
